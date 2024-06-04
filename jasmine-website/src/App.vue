925e105d9ea4e45e81512d74befe06f50da3a943
Jasmine Battestin Nunes <jasmine.battestin@gmail.com>
2024-06-03 23:18:59 -0300

    prototype complete

--- jasmine-website/src/App.vue
+++ jasmine-website/src/App.vue
@@ -8,47 +8,42 @@ import Navbar from './components/Navbar.vue';
 
   <div id="app">
     <Navbar />
-    <!-- <router-view></router-view> -->
   </div>
   
   <header>
-    <!-- <img alt="Pixel Art of Me" class="logo" src="/public/favicon.ico" width="125" height="125" /> -->
 
-    <!-- <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div> -->
   </header>
 
-  <!-- <main>
-    <TheWelcome />
-  </main> -->
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  margin-bottom: 1rem; 
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto;
 }
 
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
   header .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
   }
+
+  
 }
 </style>

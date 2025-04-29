b25e8f2fe62ed9ca0acf8616e9528435287a08f4
Jasmine Battestin Nunes <jasmine.battestin@gmail.com>
2025-04-29 16:21:54 -0300

    fix references

--- App.vue
+++ App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
-import Navbar from './components/Navbar.vue';
+import Navbar from '/jasmine-website/src/components/Navbar.vue';
 </script>
 
 <template>
